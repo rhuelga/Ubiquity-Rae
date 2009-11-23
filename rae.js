aef7f9c65ccb1d56cbf3ed211f3754a8fb60347a
Kanzeon <kanzeon@Kanzeons-MacBook-Pro.local>
2009-11-23 15:20:07 +0100

    Copy the current files to the git hub

---
+++ rae.js
@@ -0,0 +1,76 @@
+/* Roberto Huelga - rhuelga@gmail.com */
+CmdUtils.CreateCommand({
+    names: ["rae"],
+    icon: "http://www.rae.es/favicon.ico",
+    description: "Look for rae definition.",
+    help: "How to use your command.",
+    author: {name: "Roberto Huelga", email: "rhuelga@gmail.com"},
+    license: "GPL",
+    homepage: "http://labs.mozilla.com/",
+    arguments: [{role: 'object', nountype: noun_arb_text}],
+    preview: function preview(pblock, args) {
+	pblock.innerHTML = "This will show until the AJAX request returns";
+	CmdUtils.previewGet(pblock, "http://buscon.rae.es/draeI/SrvltGUIBusUsual?LEMA=" + args.object.text +"&origen=RAE&TIPO_BUS=3", function (htm) {
+	    // $(html).find('a').click(function (event){
+	    // 	$(this).attr('href', "www.google.com");
+	    // });
+            pblock.innerHTML = htm;
+	})
+    },
+    execute: function execute(args) {
+	var search_string = args.object.text;
+	
+	var windowManager = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+        .getService(Components.interfaces.nsIWindowMediator);
+	var browserWindow = windowManager.getMostRecentWindow("navigator:browser");
+	var browser = browserWindow.getBrowser();
+	browser.loadOneTab("http://buscon.rae.es/draeI/SrvltGUIBusUsual?LEMA=" + search_string +"&origen=RAE&TIPO_BUS=3", null, null, null, false, false);
+    }
+});
+
+CmdUtils.CreateCommand({
+    names: ["conjugar"],
+    icon: "http://www.rae.es/favicon.ico",
+    description: "Look for rae definition.",
+    help: "How to use your command.",
+    author: {name: "Roberto Huelga", email: "rhuelga@gmail.com"},
+    license: "GPL",
+    homepage: "http://labs.mozilla.com/",
+    arguments: [{role: 'object', nountype: noun_arb_text}],
+
+    _verbUrl: function rae__verbUrl( verb, callback_ok, callback_not_found ) {
+	$.get( "http://buscon.rae.es/draeI/SrvltGUIBusUsual?LEMA=" + verb +"&origen=RAE&TIPO_BUS=3", null, function (data, textStatus) {
+	    var verb_addr =  $(data).find('a[href^=SrvltGUIVerbos]').attr( "href" );
+	    if( verb_addr ) {
+		callback_ok( "http://buscon.rae.es/draeI/" + verb_addr );
+	    } else {
+		if( callback_not_found ) {
+		    callback_not_found();
+		}
+	    }
+	})
+    },
+    
+    previewDelay: 500,
+    preview: function preview(pblock, args) {
+	pblock.innerHTML = "Buscando en el diccionario.";
+	this._verbUrl( args.object.text,
+		       function (url) {
+			   CmdUtils.previewGet(pblock, url, function (htm) {
+			       pblock.innerHTML = htm;
+			   })},
+		       function () {
+			   pblock.innerHTML = "Verbo '" + args.object.text + "' no encontrado.";
+		       });
+    },
+    execute: function execute(args) {
+
+	this._verbUrl( args.object.text, function (url) {
+	    var windowManager = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+            .getService(Components.interfaces.nsIWindowMediator);
+	    var browserWindow = windowManager.getMostRecentWindow("navigator:browser");
+	    var browser = browserWindow.getBrowser();
+	    browser.loadOneTab( url , null, null, null, false, false);
+	})
+    }
+});
